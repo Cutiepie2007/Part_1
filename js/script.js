b4e757547ba06c523ef09da7644cf3a2929ac779
Cutiepie2007 <saaheerahmajiet100@gmail.com>
2026-06-18 20:12:32 +0200

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 /* VIDA E CAFFE _ SCRIPT.JS 
 INTERACTIVE WEBSITE FEATURES */
+
+/*CART SYSTEM*/
+
+let cart = [];
+let Total = 0;
+
+const prices = {
+    "Espresso": 25.00,
+    "Cappuccino": 35.00,
+    "Latte": 40.00,
+    "lemonade": 38.00,
+    "Smoothie": 45.00,
+    "Muffin": 20.00,
+    "Sandwich": 50.00,
+};
+
+function addToCart(product) {
+    cart.push(product);
+    if (prices[product]) {
+        Total += prices[product];
+    } 
+    
+    updateCartDisplay();
+    saveCart();
+ }
+
+function updateCartDisplay() {
+    const CartDisplay = document.getElementById("cart");
+
+    if (CartDisplay) {
+        CartDisplay.innerHTML = 
+        `Your Order: ${cart.length} item(s) | Total:R$ {Total}`;
+    }
+
+    
+}
